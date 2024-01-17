dd1ca16a28cac4cf0a2c017ec60216d3254317e0
Lucie <strakova.91@gmail.com>
2024-01-17 15:34:13 +0100

    fixed bug with naming of the travels

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,13 @@
 export const formatTitleToURL = (title: string) => {
+
+  // czech special characters
+  const accentsMap: { [key: string]: string } = { á: 'a', č: 'c', ď: 'd', é: 'e', ě: 'e', í: 'i', ň: 'n', ó: 'o', ř: 'r', š: 's', ť: 't', ú: 'u', ů: 'u', ý: 'y', ž: 'z' };
+  
   return title
     .toLowerCase()
+    .split('')
+    .map(char => accentsMap[char] || char)
+    .join('')
     .replace(/\s+/g, '-') // Replace spaces with hyphens
     .replace(/[^\w-]+/g, ''); // Remove all non-word chars
 };
